267d94099d6a8ea70e72bfec41aa1559fc3f7564
YaKuBoVO <oyakubov526@gmail.com>
2023-05-07 23:21:34 +0500

    Pashti tayyor

--- script.js
+++ script.js
@@ -2,4 +2,11 @@ elDarkLightBtn = document.querySelector('#dark-light-btn');
 
 elDarkLightBtn.addEventListener('click', function() {
    document.body.classList.toggle('dark');
-});
+})
+
+
+IconMenu = document.querySelector(".icon-menu");
+IconMenu.onclick = function() {
+   navbarList = document.querySelector(".navbar-list");
+   navbarList.classList.toggle("active");
+}
